7f245e01ae805c939f0b6876d2a9496cf69f2a3a
s27788 <s27788@pjwstk.edu.pl>
2026-04-15 16:37:06 +0200

    BREW-106: Add DTO for Coffee detail response

---
+++ brewbase/backend/brewbase.server/Dtos/CoffeeDetailResponseDto.cs
@@ -0,0 +1,20 @@
+namespace brewbase.server.Dtos;
+
+public class CoffeeDetailResponseDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public bool IsVerified { get; set; }
+
+    public string? Region { get; set; }
+
+    public string? Roastery { get; set; }
+
+    public string? ProcessingMethod { get; set; }
+
+    public string? Variety { get; set; }
+
+    public int CreatedByUserId { get; set; }
+}
