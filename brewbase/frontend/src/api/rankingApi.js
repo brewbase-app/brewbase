4c7786fbf934d983ff3d69066383fd30c3e0735f
Jakub Erbetowski <s27747@pjwstk.edu.pl>
2026-05-14 20:36:27 +0200

    Connect user ranking frontend

--- brewbase/frontend/src/api/rankingApi.js
+++ brewbase/frontend/src/api/rankingApi.js
@@ -3,3 +3,7 @@ import { apiRequest } from "./apiClient";
 export function getCoffeeRanking() {
     return apiRequest("/api/Ranking/coffees");
 }
+
+export function getUserRanking() {
+    return apiRequest("/api/Ranking/users");
+}
