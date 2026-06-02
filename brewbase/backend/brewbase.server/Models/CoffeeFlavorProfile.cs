2a571085d1d7d442ccd503aadb140818c14d5517
Olaf Chodkiewicz <s26662@pjwstk.edu.pl>
2026-06-02 23:48:52 +0200

    Poprawienie błędów

---
+++ brewbase/backend/brewbase.server/Models/CoffeeFlavorProfile.cs
@@ -0,0 +1,14 @@
+﻿using brewbase.server.Models;
+
+namespace DefaultNamespace;
+
+public partial class CoffeeFlavorProfile
+{
+    public int CoffeeId { get; set; }
+
+    public int FlavorProfileId { get; set; }
+
+    public virtual Coffee Coffee { get; set; } = null!;
+
+    public virtual FlavorProfile FlavorProfile { get; set; } = null!;
+}
--- brewbase/database/schema.sql
+++ brewbase/database/schema.sql
@@ -328,6 +328,16 @@ CREATE TABLE report (
                         CONSTRAINT report_user_fk FOREIGN KEY (reported_by_user_id) REFERENCES app_user(id)
 );
 
+CREATE TABLE coffee_flavor_profile
+(
+    coffee_id INT NOT NULL,
+    flavor_profile_id INT NOT NULL,
+    CONSTRAINT pk_coffee_flavor_profile PRIMARY KEY (coffee_id, flavor_profile_id),
+    CONSTRAINT fk_coffee_flavor_profile_coffee FOREIGN KEY (coffee_id) REFERENCES coffee(id) ON DELETE CASCADE,
+    CONSTRAINT fk_coffee_flavor_profile_flavor_profile  FOREIGN KEY (flavor_profile_id) REFERENCES flavor_profile(id) ON DELETE CASCADE
+);
+
+
 -- =====================
 -- Foreign keys
 -- =====================
